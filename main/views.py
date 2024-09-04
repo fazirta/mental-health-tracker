28cd736ad1489108331a686b7c1c1dcc1064a9bd
Fazil <faziltirtana@tutanota.com>
2024-09-04 13:13:23 +0700

    feat: connect view to template and configure URL routing

--- main/views.py
+++ main/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+def show_main(request):
+    context = {
+        'npm' : '2306274983',
+        'name': 'Muhammad Fazil Tirtana',
+        'class': 'PBP D'
+    }
+
+    return render(request, "main.html", context)
