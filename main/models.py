a9e01a48a1c845aa01a2f027fe19e7d9d26e06b2
Fazil <faziltirtana@tutanota.com>
2024-09-18 15:17:44 +0700

    feat: add session and cookie authentication

--- main/models.py
+++ main/models.py
@@ -1,15 +1,15 @@
+from django.contrib.auth.models import User
 from django.db import models
 import uuid
 
 
 class MoodEntry(models.Model):
-    id = models.UUIDField(
-        primary_key=True, default=uuid.uuid4, editable=False
-    )
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     mood = models.CharField(max_length=255)
     time = models.DateField(auto_now_add=True)
     feelings = models.TextField()
     mood_intensity = models.IntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     @property
     def is_mood_strong(self):
