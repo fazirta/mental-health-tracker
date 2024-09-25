be5cc9995cae3df256d9a032064f9a3308658eff
Fazil <faziltirtana@tutanota.com>
2024-09-25 15:35:31 +0700

    feat: add delete mood feature

--- main/urls.py
+++ main/urls.py
@@ -8,6 +8,7 @@ from main.views import (
     show_xml_by_id,
     show_json_by_id,
     edit_mood,
+    delete_mood,
 )
 from main.views import register
 from main.views import login_user
@@ -20,10 +21,11 @@ urlpatterns = [
     path("create-mood-entry", create_mood_entry, name="create_mood_entry"),
     path("xml/", show_xml, name="show_xml"),
     path("json/", show_json, name="show_json"),
-    path("xml/<str:id>/", show_xml_by_id, name="show_xml_by_id"),
-    path("json/<str:id>/", show_json_by_id, name="show_json_by_id"),
+    path("xml/<uuid:id>/", show_xml_by_id, name="show_xml_by_id"),
+    path("json/<uuid:id>/", show_json_by_id, name="show_json_by_id"),
     path("register/", register, name="register"),
     path("login/", login_user, name="login"),
     path("logout/", logout_user, name="logout"),
     path('edit-mood/<uuid:id>', edit_mood, name='edit_mood'),
+    path('delete/<uuid:id>', delete_mood, name='delete_mood'),
 ]
